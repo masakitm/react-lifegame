1bdfbe1d98451a60f10aa64eeda1373305995a40
友塚　正樹 <uu144692@CA4167.local>
2019-05-15 10:54:18 +0900

    babel + webpack

---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: __dirname + '/dist',
+    publicPath: '/',
+    filename: 'bundle.js'
+	},
+	module: {
+		rules: [
+			{
+				test: /\.(.js|.jsx)$/,
+				exclude: /node_modules/,
+				use: ['babel-loader']
+			}
+		]
+	},
+	resolve: {
+		extensions: ['*', '.js', '.jsx']
+	},
+  devServer: {
+		contentBase: './dist',
+		port: 3008
+	}
+};
